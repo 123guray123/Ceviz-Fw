9d0999b31c37056a9664f3972ab39019767a881d
Yusuf Koç <raiden@ceviz.net>
2011-01-13 04:38:15 +0200

    dosyalar

---
+++ system/controller/BaseController.php
@@ -0,0 +1,23 @@
+<?php
+defined('SITE_PATH') or die('No direct access');
+
+/**
+ * Ana Kontroller.
+ * 
+ * @package Ceviz Framework
+ * @author Yusuf Koç
+ * @copyright Copyright (c) 2001 - 2011 Ceviz.Net
+ * @link http://www.ceviz.net - http://ysfkc.com
+ * @since Version 1.0
+ * @license See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ */
+
+abstract class BaseController
+{
+    abstract function index();
+
+    //TODO: İlerde before koyulabilir
+    //TODO: İlerde after koyulabilir.
+}
+
+// End of file BaseController
---
+++ system/model/BaseModel.php
@@ -0,0 +1,36 @@
+<?php
+defined('SITE_PATH') or die('No direct access');
+
+/**
+ * Ana Model.
+ *
+ * @package Ceviz Framework
+ * @author Yusuf Koç
+ * @copyright Copyright (c) 2001 - 2011 Ceviz.Net
+ * @link http://www.ceviz.net - http://ysfkc.com
+ * @since Version 1.0
+ * @license See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ */
+
+class BaseModel
+{
+    protected $MasterDatabase;
+    protected $SlaveDatabase;
+    
+    public function __construct()
+    {
+        $config = Config::load('database');
+
+        if ($config['master']['active'])
+        {
+            $this->MasterDatabase = Database::instance('master');
+        }
+
+        if ($config['slave']['active'])
+        {
+            $this->SlaveDatabase = Database::instance('slave');
+        }
+    }
+}
+
+// End of file BaseModel
